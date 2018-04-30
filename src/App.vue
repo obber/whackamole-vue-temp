bf7e161b03977a44e4a5d54f648ab6e1eed72403
Kan Adachi <kan.adachi@am.sony.com>
2018-04-30 15:50:17 -0700

    phase3

--- src/App.vue
+++ src/App.vue
@@ -57,8 +57,8 @@ export default {
     // control game flow
     startRound: function () {
       this.timer = 20;
-      this.whacks = 0;
-      this.gameActive = true;
+      this.whacks = undefined; // fix me
+      this.gameActive = undefined; // fix me
       this.deactivateAllMoles();
       this.startTimer();
       this.startMoles();
@@ -68,56 +68,42 @@ export default {
       this.stopTimer();
       this.stopMoles();
       this.deactivateAllMoles();
-      this.highScore = Math.max(whacks, highScore);
-      this.gameActive = false;
+      this.highScore = 99; // fix me
+      this.gameActive = undefined; // fix me
     },
 
     // timer logic
     startTimer: function () {
-      this.timerInterval = setInterval(this.decrementTime.bind(this), 1000);
+
     },
     stopTimer: function () {
-      clearInterval(this.timerInterval);
+
     },
     startMoles: function () {
-      this.moleInterval = setInterval(this.activateRandomMole.bind(this), 500);
+
     },
     stopMoles: function () {
-      clearInterval(this.moleInterval);
+
     },
     decrementTime: function () {
-      this.timer = this.timer - 1;
-      if (this.timer === 0) {
-        this.endRound();
-      }
+
     },
 
     // mole logic
     incrementWhacks: function (moleId) {
-      const moles = this.moles.slice();
-      moles[moleId] = false;
-      this.whacks = this.whacks + 1;
-      this.moles = moles;
+
     },
     activateRandomMole: function () {
-      const randomMoleIndex = Math.floor(Math.random() * this.moles.length);
-      if (!this.moles[randomMoleIndex]) {
-        this.activateMole(randomMoleIndex);
-      }
+
     },
     activateMole: function (id) {
-      const moles = this.moles.slice();
-      moles[id] = true;
-      this.moles = moles;
-      setTimeout(() => this.deactivateMole(id), 1500);
+
     },
     deactivateMole: function (id) {
-      const moles = this.moles.slice();
-      moles[id] = false;
-      this.moles = moles;
+
     },
     deactivateAllMoles: function () {
-      this.moles = this.moles.map(() => false);
+
     }
   }
 }
